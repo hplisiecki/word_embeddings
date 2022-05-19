edf1092e86dfb11b6fdd5dbece73ac7453073f0d
viktoriia-muliavka <80672391+viktoriia-muliavka@users.noreply.github.com>
2022-05-19 20:38:18 +0800

    change_vik

--- word2vec.py
+++ word2vec.py
@@ -7,6 +7,7 @@ sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
 			['yet', 'another', 'sentence'],
 			['one', 'more', 'sentence'],
 			['and', 'the', 'final', 'sentence']]
+#####################
 # train model
 model = Word2Vec(sentences, min_count=1)
 # summarize the loaded model
